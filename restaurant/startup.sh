5c72106e4a20b1ccd8042cd8415201cc9fb4baea
MischaGe <gemsa.michael@gmail.com>
2023-04-09 11:25:08 +0200

    test9

--- restaurant/startup.sh
+++ restaurant/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PROJECTNAME=restaurant
-PORT=8000
+PORT=80
 PROJECTDIR="/app/restaurant"
 
 python3 restaurant/manage.py migrate
@@ -8,11 +8,11 @@ python3 restaurant/manage.py migrate
 echo "Starting up uwsgi on $IP_ADDRESS:$PORT as $VIRTUAL_HOST"
 
 uwsgi --plugins python3 \
+      --chdir $PROJECTDIR \
+      --wsgi $PROJECTNAME.wsgi:application \
+      --env DJANGO_SETTINGS_MODULE=$PROJECTNAME.settings \
       --uid 1000 \
       --gid 1000 \
-      --chdir "$PROJECTDIR" \
-      --wsgi "$PROJECTNAME.wsgi:application" \
-      --env "DJANGO_SETTINGS_MODULE=$PROJECTNAME.settings" \
       --http-socket "0.0.0.0:$PORT" \
       --check-static "/app" \
       --vacuum
